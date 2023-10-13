37546b7f605f551cc6363cb1aed9425a58b1d4b6
northsea4 <ns4@github.io>
2023-10-13 10:03:08 +0800

    fix /cert-patch permission

--- gui-base/rootfs/etc/cont-init.d/250-do-me-a-favor.sh
+++ gui-base/rootfs/etc/cont-init.d/250-do-me-a-favor.sh
@@ -4,4 +4,9 @@ if command -v take-ownership > /dev/null 2>&1; then
   echo "😁 take-ownership: /app and /mdcx-config"
   take-ownership /app
   take-ownership /mdcx-config
+  
+  if [ -d "/cert-patch" ]; then
+    echo "😁 take-ownership: /cert-patch"
+    take-ownership /cert-patch
+  fi
 fi
